bf95477df9d4cbad031099d5aa96c798bc74b499
PrajyotGajane <prajyotgajane@gmail.com>
2020-08-16 22:34:18 +0530

    Added: Logic to calculate daily employee wage

--- EmployeeWageProblem.cpp
+++ EmployeeWageProblem.cpp
@@ -1,21 +1,36 @@
+// UC2: Calculate Daily Employee Wage 
+// 	- Assume Wage per Hour is 20 
+// 	- Assume Full Day Hour is 8
 #include <iostream>
 #include <time.h>
 using namespace std;
 int checkAttendance();
+int wageCalculator();
+int wagePerHour = 20;
+int fullDayHour = 8;
 int main() {
     int isPresent = 1;
     int isAbsent = 2;
+    int employeeWage;
     cout << "\n\n\n------------------------------------\n\n";
     cout << "\nWelcome to Employee Wage Problem\n" << endl;
     int attendance = checkAttendance();
-    if (attendance == isPresent) 
+    if (attendance == isPresent) {
         cout << "Employee is present" << endl;
+        employeeWage = wageCalculator();
+        cout << "Employees wage for today : " << employeeWage << endl;
+    }   
     else 
         cout << "Employee is absent" << endl;
+    
+    
     return 0;
 }
-
 int checkAttendance(){
     srand(time(NULL));
 	return ((rand() % 2) + 1);
+}
+
+int wageCalculator(){
+    return wagePerHour * fullDayHour;
 }
